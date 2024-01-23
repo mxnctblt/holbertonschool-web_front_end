77704462aa3e2ee3b5f087c2b80a885f0898b45c
mxnc <thibaultmaxence44@gmail.com>
2024-01-23 18:14:37 +0100

    task 14

---
+++ Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,18 @@
+function createElement(data) {
+    const paragraph = document.createElement('p');
+    paragraph.innerHTML = data;
+    document.body.appendChild(paragraph);
+}
+function queryWikipedia(callback) {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = () => {
+        if (request.readyState == XMLHttpRequest.DONE) {
+        response = JSON.parse(request.responseText);
+        callback(response.query.pages['21721040'].extract)
+        }
+    }
+    request.open('GET', 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*', true);
+    request.send();
+    return request
+}
+queryWikipedia(createElement);
